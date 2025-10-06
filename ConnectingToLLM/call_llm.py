68143c70a83466e5beabeab0f803c6d5ffabf79c
big-Bong <vasuchat@gmail.com>
2025-10-06 19:08:51 +0530

    Experimented with gemini

--- ConnectingToLLM/call_llm.py
+++ ConnectingToLLM/call_llm.py
@@ -5,7 +5,7 @@ import os
 
 load_dotenv()
 
-model = init_chat_model("gpt-4o-mini",model_provider="openai")
+model = init_chat_model("gemini-2.5-flash",model_provider="google_genai")
 messages = [
     SystemMessage(content="You are an intelligent and helpful assistant, and try to give fact based answer to the questions you are asked.\
                   If you don't know the answer, then you will say you don't know the answer. Please answer the following question."),
