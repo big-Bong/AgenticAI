79614a05ebcd4241e8ef9614b50761798322812d
big-Bong <vasuchat@gmail.com>
2025-10-09 00:18:54 +0530

    Added limited memory of conversation

--- MultiturnConversation/template_and_trimming.py
+++ MultiturnConversation/template_and_trimming.py
@@ -2,7 +2,7 @@
 
 from dotenv import load_dotenv
 from langchain.chat_models import init_chat_model
-from langchain_core.messages import SystemMessage, HumanMessage
+from langchain_core.messages import SystemMessage, HumanMessage, trim_messages
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langgraph.checkpoint.memory import MemorySaver
 from langgraph.graph import START, MessagesState ,StateGraph
@@ -20,10 +20,27 @@ prompt_template = ChatPromptTemplate.from_messages(
         MessagesPlaceholder(variable_name="messages")
     ]
 )
+trimmer = trim_messages(
+    max_tokens=250,
+    strategy="last",
+    token_counter=model,
+    include_system=True,
+    allow_partial=False,
+    start_on="human",
+)
+messages = [
+    SystemMessage(content="You are a good therapist and like talking to people and making them comfortable. You try to understand \
+            what a person is going through by talking with them.")
+]
 workflow = StateGraph(state_schema=MessagesState)
 
 def call_model(state: MessagesState):
-    prompt = prompt_template.invoke(state)
+    print(f"Messages before trimming: {len(state['messages'])}")
+    trimmed_messages = trimmer.invoke(state["messages"])
+    print(f"Messages after trimming: {len(trimmed_messages)}")
+    prompt = prompt_template.invoke(
+        {"messages":trimmed_messages}
+    )
     response = model.invoke(prompt)
     return {"messages": response}
 
@@ -36,7 +53,7 @@ config = {"configurable": {"thread_id": "abc123"}}
 
 query = input("User: ")
 while query != "bye":
-    input_messages = [HumanMessage(query)]
+    input_messages = messages + [HumanMessage(query)]
     output = app.invoke({"messages":input_messages}, config)
     output["messages"][-1].pretty_print()
     query = input("User: ")
