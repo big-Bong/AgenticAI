6b79add0a463b2820f834a9f13cd6c0a0058fad7
big-Bong <vasuchat@gmail.com>
2025-10-11 15:03:21 +0530

    Setting up basic rag for web blog

---
+++ RAG/basic_rag.py
@@ -0,0 +1,43 @@
+from dotenv import load_dotenv
+from langchain import hub
+from langchain.chat_models import init_chat_model
+from langchain_community.document_loaders import WebBaseLoader
+from langchain_core.documents import Document
+from langchain_core.vectorstores import InMemoryVectorStore
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langgraph.graph import START, StateGraph
+from typing_extensions import List, TypedDict
+
+import bs4
+
+load_dotenv()
+
+model = init_chat_model(model="gemini-2.5-flash",model_provider="google_genai")
+embeddings = GoogleGenerativeAIEmbeddings(model="models/gemini-embedding-001")
+vector_store = InMemoryVectorStore(embedding=embeddings)
+
+loader = WebBaseLoader(
+    web_path=("https://lilianweng.github.io/posts/2023-06-23-agent/",),
+    bs_kwargs=dict(
+        parse_only=bs4.SoupStrainer(
+            class_=("post-content", "post-title", "post-header")
+        )
+    ),
+)
+docs = loader.load()
+
+text_splitter = RecursiveCharacterTextSplitter(
+    chunk_size=1000,
+    chunk_overlap=200,
+    add_start_index=True,
+)
+all_splits = text_splitter.split_documents(docs)
+
+_ = vector_store.add_documents(documents=all_splits)
+prompt = hub.pull("rlm/rag-prompt")
+
+class State(TypedDict):
+    question: str
+    context: List[Document]
+    answer: str
